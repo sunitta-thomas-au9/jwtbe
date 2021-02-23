d4cc560c9ba23982051ad93a4b9ff2d9675157f0
SunittaT <sunitta.thomas@outlook.com>
2021-02-23 11:51:12 +0530

    update mongoAtlas

--- db/db.js
+++ db/db.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Sunitta:sunitta123@cluster0.pzgp6.mongodb.net/travelApp?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://Sunitta:sunitta123@cluster0.pzgp6.mongodb.net/travelApp?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://amazedUser:amazed123@cluster0.yocxz.mongodb.net/AmazedApp?retryWrites=true&w=majority');
